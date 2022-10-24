cbec0dca075b929a001ac663c6d0e90e6735603b
Tanner Van Amen <Tvanamen@yahoo.com>
2022-10-24 17:22:24 -0600

    intial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+namespace LINQ  
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var videoGames = new List<string> { "MineCraft", "GTA 5", "BioShock", "Call of Duty", "Portal", "Mass Effect", "Half-Life", "Zelda", "Super Mario", "God of War" };
+            var orderOfGames = videoGames.OrderBy(x => x.Length);
+            foreach(var game in orderOfGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
